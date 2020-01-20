1916653a2684ec20fa9adf303076466d1973ec3c
Meghana Chillal <meghana.chillal@spglobal.com>
2020-01-20 16:24:17 -0500

    The first check in for Microservices in Asp.net Core

---
+++ AdvertApi/AdvertApi/Interfaces/IAdvertStorageService.cs
@@ -0,0 +1,16 @@
+﻿using AdvertApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Interfaces
+{
+    public interface IAdvertStorageService
+    {
+
+        Task<string> Add(AdvertModel model);
+
+        Task Confirm(ConfirmAdvertModel model);
+    }
+}
